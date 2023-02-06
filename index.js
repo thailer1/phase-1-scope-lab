04b8da3ad953f75de61a7dddf3d2452b7a3f2d9c
Thailer <thailer.palmer@gmail.com>
2023-02-06 16:30:59 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = 'BOB'
+}
+
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+
+overwriteBestCustomer = () => {
+ bestCustomer ='maybe bob'
+}
+
+const leastFavoriteCustomer = 'billy'
+
+ changeLeastFavoriteCustomer = () =>{
+    return leastFavoriteCustomer = 'bob'
+}
